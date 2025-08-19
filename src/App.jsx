e9e302d1fc9ef095c634f54f2073de072ecdf001
akmalzidani <akmal20031003@gmail.com>
2025-08-19 23:01:52 +0700

    Add styled-components and implement global styles; enhance App component with buttons and input fields

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const StyledApp = styled.main`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <StyledApp>
+      <GlobalStyles />
+      <H1>Hello World</H1>
+      <Button onClick={() => alert("Checked In")}>Check In</Button>
+      <Button onClick={() => alert("Checked Out!")}>Check Out</Button>
+
+      <Input type="number" placeholder="Number of Guests" />
+      <Input type="number" placeholder="Number of Guests" />
+    </StyledApp>
+  );
 }
 
 export default App;
