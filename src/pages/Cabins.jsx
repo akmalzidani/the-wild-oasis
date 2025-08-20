ebb0ece193d1c505646e9e8b24aa42fda7682682
akmalzidani <akmal20031003@gmail.com>
2025-08-20 23:35:57 +0700

    feat: integrate Supabase for cabin data fetching and update Cabins component

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,11 +1,38 @@
+import { useEffect, useState } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
+  const [cabins, setCabins] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const cabins = await getCabins();
+        setCabins(cabins);
+      } catch (error) {
+        console.error("Error fetching cabins:", error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
+      {!cabins.length ? (
+        <p>No cabins found</p>
+      ) : (
+        cabins.map((cabin) => (
+          <p key={cabin.id}>
+            {cabin.name}|{cabin.regularPrice}|(Discount: {cabin.discount}) (Max
+            capacity: {cabin.maxCapacity})
+          </p>
+        ))
+      )}
+      <p>{cabins.length} cabins found</p>
     </Row>
   );
 }
