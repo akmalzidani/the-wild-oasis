81ca98e867473ea079331ca8fee34cdd4b82b63d
akmalzidani <akmal20031003@gmail.com>
2025-08-22 08:25:54 +0700

    feat: enhance cabin creation by adding image upload handling and validation in CreateCabinForm

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://vrhnpwciraajgxmdulun.supabase.co";
+export const supabaseUrl = "https://vrhnpwciraajgxmdulun.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZyaG5wd2NpcmFhamd4bWR1bHVuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU3MDUwODgsImV4cCI6MjA3MTI4MTA4OH0.7ftuvUCHBl6AKM4i_kVKAlpiEUshPrr3y4oYsIfC64M";
 const supabase = createClient(supabaseUrl, supabaseKey);
