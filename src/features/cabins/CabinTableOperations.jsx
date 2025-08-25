7c04f0f7f05bdd336117de6495010ccb9c907ef2
akmalzidani <akmal20031003@gmail.com>
2025-08-25 18:07:40 +0700

    feat: implement filtering functionality in CabinTable; add CabinTableOperations component for discount filtering

---
+++ src/features/cabins/CabinTableOperations.jsx
@@ -0,0 +1,19 @@
+import TableOperations from "../../ui/TableOperations";
+import Filter from "../../ui/Filter";
+
+function CabinTableOperations() {
+  return (
+    <TableOperations>
+      <Filter
+        filterField="discount"
+        options={[
+          { label: "All", value: "all" },
+          { label: "No Discount", value: "no-discount" },
+          { label: "With Discount", value: "with-discount" },
+        ]}
+      />
+    </TableOperations>
+  );
+}
+
+export default CabinTableOperations;
